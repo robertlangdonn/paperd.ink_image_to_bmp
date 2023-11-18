c3fd24792b36d18f2e08ca69b8e43d83d928fd1e
Prasad Khake <prasad@paperd.ink>
2023-11-19 04:16:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ function generateOutputPath(inputPath, settings) {
     const baseName = path.basename(inputPath, extension);
     const monoOrColor = settings.convertToMonochrome ? 'mono' : 'color';
     const timestamp = new Date().getTime();
-    return path.join(outputDir, `dithered-${baseName}-${settings.margin}m-${monoOrColor}-${timestamp}${extension}`);
+    return path.join(outputDir, `dithered-${baseName}-${settings.margin}m-${monoOrColor}-${timestamp}.bmp`);
 }
 
 function processImage(imagePath, settings, callback) {
@@ -23,7 +23,17 @@ function processImage(imagePath, settings, callback) {
     Jimp.read(imagePath)
         .then(image => resizeAndDitherImage(image, innerWidth, innerHeight, settings.ditheringMethod, settings.convertToMonochrome))
         .then(ditheredImage => addMargins(ditheredImage, settings.targetWidth, settings.targetHeight, settings.margin))
-        .then(finalImage => finalImage.write(outputPath, callback))
+        .then(finalImage => {
+            // Convert the processed image to BMP format
+            finalImage.write(outputPath, (err) => {
+                if (err) {
+                    console.error(err);
+                    if (callback) callback(err);
+                } else {
+                    if (callback) callback(null, outputPath);
+                }
+            });
+        })
         .catch(err => {
             console.error(err);
             if (callback) callback(err);
@@ -39,18 +49,20 @@ function processImagesFromDirectory(inputDir, settings) {
 
         files.forEach(file => {
             const filePath = path.join(inputDir, file);
-            if (path.extname(file).toLowerCase() === '.jpg' || path.extname(file).toLowerCase() === '.png') {
-                processImage(filePath, settings, (err) => {
+            const extname = path.extname(file).toLowerCase();
+            if (extname === '.jpg' || extname === '.png') {
+                processImage(filePath, settings, (err, outputPath) => {
                     if (err) {
                         console.error(`Error processing image ${filePath}:`, err);
                     } else {
-                        console.log(`Image ${filePath} processed and saved as ${generateOutputPath(filePath, settings)}`);
+                        console.log(`Image ${filePath} processed and saved as ${outputPath}`);
                     }
                 });
             }
         });
     });
 }
+
 
 // Create output directory if it doesn't exist
 if (!fs.existsSync(outputDir)){
@@ -61,7 +73,7 @@ if (!fs.existsSync(outputDir)){
 const settings = {
     targetWidth: 400,
     targetHeight: 300,
-    margin: 0,
+    margin: 5,
     ditheringMethod: 'dither565',
     convertToMonochrome: false
 };
