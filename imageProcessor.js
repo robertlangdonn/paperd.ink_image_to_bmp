c32f28496158c8f0d432ad28945c7e84c434966e
Prasad Khake <prasad@paperd.ink>
2023-11-19 05:45:31 +0530

    Update imageProcessor.js

--- imageProcessor.js
+++ imageProcessor.js
@@ -2,7 +2,7 @@ const Jimp = require('jimp');
 const resizeAndDitherImage = require('./resizeAndDitherImage');
 const addMargins = require('./addMargins');
 
-function processImage(imagePath, settings) {
+function processImage(imagePath, settings, outputPath) {
     return new Promise((resolve, reject) => {
         const innerWidth = settings.targetWidth - (settings.margin * 2);
         const innerHeight = settings.targetHeight - (settings.margin * 2);
@@ -10,12 +10,8 @@ function processImage(imagePath, settings) {
         Jimp.read(imagePath)
             .then(image => resizeAndDitherImage(image, innerWidth, innerHeight, settings.ditheringMethod, settings.convertToMonochrome))
             .then(ditheredImage => addMargins(ditheredImage, settings.targetWidth, settings.targetHeight, settings.margin))
-            .then(finalImage => {
-                finalImage.getBase64(Jimp.MIME_BMP, (err, data) => {
-                    if (err) reject(err);
-                    else resolve(data);
-                });
-            })
+            .then(finalImage => finalImage.writeAsync(outputPath))
+            .then(() => resolve(outputPath))
             .catch(err => reject(err));
     });
 }
